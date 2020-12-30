4a90effa205e6f0a02e7c26872da11618c21bc6f
MKemp <mkemp@accu-time.com>
2020-12-30 14:51:01 -0500

    Add new student activity and fill in layouts and strings

---
+++ app/src/main/java/com/mkemp/studentregister/AddNewStudentActivity.java
@@ -0,0 +1,16 @@
+package com.mkemp.studentregister;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddNewStudentActivity extends AppCompatActivity
+{
+    
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_student);
+    }
+}
