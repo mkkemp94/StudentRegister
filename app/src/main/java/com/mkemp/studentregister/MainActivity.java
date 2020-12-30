0ef5847cd6cea543e33b521e0ccc6f5b0f900c53
MKemp <mkemp@accu-time.com>
2020-12-30 14:40:26 -0500

    Add dependencies for room

--- app/src/main/java/com/mkemp/studentregister/MainActivity.java
+++ app/src/main/java/com/mkemp/studentregister/MainActivity.java
@@ -1,6 +1,9 @@
 package com.mkemp.studentregister;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -8,14 +11,8 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
-
-import android.view.Menu;
-import android.view.MenuItem;
-
 public class MainActivity extends AppCompatActivity
 {
-    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
